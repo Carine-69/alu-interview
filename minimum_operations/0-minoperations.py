e1e8e5c45eda64007403ce18813a937768984329
carine-69 <umugabecarine@gmail.com>
2024-09-13 21:56:58 +0200

    created a result files

--- minimum_operations/0-minoperations.py
+++ minimum_operations/0-minoperations.py
@@ -3,6 +3,16 @@ import os
 
 
 def minOperations(n):
+    """
+    Calculate the minimum number of operations needed to achieve exactly n 'H' characters
+    starting from one 'H' using Copy All and Paste operations.
+
+    Parameters:
+    n (int): The target number of 'H' characters.
+
+    Returns:
+    int: The minimum number of operations needed, or 0 if n is less than or equal to 1.
+    """
     if n <= 1:
         return 0  # No operations are needed if n is less than or equal to 1
 
@@ -22,14 +32,15 @@ def minOperations(n):
 # Example usage:
 
 
-test = [9,102345]   # example
+test = [25, 1010110,120]  # List of test cases to evaluate
 
 for i, case in enumerate(test):
-    result = minOperations(case)
+    result = minOperations(case)  # Compute the result for the current test case
 
-    filename = f"main-{i}.py"
+    filename = f"main-{i}.py"  # Create a filename based on the index of the test case
 
     with open(filename, "w") as file:
-        file.write(f"result\n")
+        file.write(f"Min number of operations to reach {case} characters:{result}\n")  # Write the string "result\n"
+        # to the file
 
-    print(f"result of test {result} written to {filename}")
+    print(f"Result of test case {case} is written to {filename}")
